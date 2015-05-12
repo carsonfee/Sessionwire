18daf1891b9b598900c21961d718e31f6774f86f
carseomighty@gmail.com <carseomighty@Carsons-MacBook-Pro.local>
2015-05-11 20:46:57 -0500

    Initial Commit

---
+++ Session Wire/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Session Wire
+//
+//  Created by Carson Fee on 5/11/15.
+//  Copyright (c) 2015 Carson Fee. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Session Wire/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Session Wire
+//
+//  Created by Carson Fee on 5/11/15.
+//  Copyright (c) 2015 Carson Fee. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
